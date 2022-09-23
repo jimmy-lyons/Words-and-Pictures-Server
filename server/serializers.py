c82f8c41ae320e20b3ede5ae9c146fb204100b6b
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-09-23 15:27:57 +0100

    updated serializer to include missing data

--- server/serializers.py
+++ server/serializers.py
@@ -4,5 +4,5 @@ from .models import Project
 class ProjectSerializer(serializers.ModelSerializer):
   class Meta:
     model = Project
-    fields = ['id', 'name', 'description']
+    fields = ['id', 'name', 'heading', 'client', 'description']
     
