ef1f8091be7a4de8c062bc8390aceb2d2261462c
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-09-26 19:04:18 +0100

    updated views

--- server/views.py
+++ server/views.py
@@ -1,14 +1,17 @@
 from django.http import JsonResponse 
 from .models import Project, ProjectImage
 from .serializers import ProjectSerializer, ImageSerializer
+from rest_framework.decorators import api_view
 
 def project_list(request):
   projects = Project.objects.all()
   serializer = ProjectSerializer(projects, many=True)
   return JsonResponse(serializer.data, safe=False)
 
+@api_view(['POST',])
 def project_images(request):
-  images = ProjectImage.objects.filter(projectId=1)
+  data = request.data
+  images = ProjectImage.objects.filter(projectId=data['id'])
   serializer = ImageSerializer(images, many=True)
   return JsonResponse(serializer.data, safe=False)
   
